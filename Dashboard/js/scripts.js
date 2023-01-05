2aad3a8dd207f4beae256983aa886a204261f0c5
darlenesm <72404983+darlenesm@users.noreply.github.com>
2023-01-04 21:14:40 -0400

    Add files via upload

---
+++ Dashboard/js/scripts.js
@@ -0,0 +1,160 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+  series: [{
+    data: [10, 8, 6, 4, 2],
+    name: "Products",
+  }],
+  chart: {
+    type: "bar",
+    background: "transparent",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: [
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  stroke: {
+    colors: ["transparent"],
+    show: true,
+    width: 2
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  xaxis: {
+    categories: ["Python", "JavaScript", "Go", "TypeScript", "C++"],
+    title: {
+      style: {
+        color: "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      show: true,
+      color: "#55596e",
+    },
+    axisTicks: {
+      show: true,
+      color: "#55596e",
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Count",
+      style: {
+        color:  "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+// PIE CHART
+      
+
+      
+var options = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+    type: 'donut',
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#pie-chart"), options);
+  chart.render();
